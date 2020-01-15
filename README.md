d58d23e2178063de9f7e3eb3b3e1b46ded252399
Nick Stumpf <nick.k.stumpf@gmail.com>
2020-01-15 00:34:20 -0500

    get request for id working

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ITA_FinalProject
+
+TODO:
+    1. [X] Add more products to DB
+    2. [X] Modify products table to contain URL for images
+    3. [X] Modify products table to contain "categories" field
+    4. [X] Modify products table to contain "img-url" fields (img-main, img-B, img-C)
+    5. Create filter for individual categories that will show/hide correct products on page
+    6. [X] Create GET function for individual products
+    7. [X] Update Go API to handle GET id request
+
+    8. [] Build out POST functionality
+    9. [] Capture user data and save to DB
+    10.[] Allproducts page should automatically load all products 
+    11.[] Add "Filter By..." styling to allproducts page 
+    12.[] Category buttons should take you to allproducts page with correct category loaded
+    13.[] Fix styling on "view" buttons
--- main.go
+++ main.go
@@ -34,9 +34,13 @@ func enableCors(w *http.ResponseWriter) { // cross origin resource sharing
 // Product : this is the correct format for  commment of of a struct
 type Product struct {
 	ID          string `json:"id"`
+	Category    string `json:"category"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	Price       string `json:"price"`
+	ImgMain     string `json:"img_main"`
+	ImgB        string `json:"img_b"`
+	ImgC        string `json:"img_c"`
 }
 
 // Initialize products variable as a slice of Product struct
@@ -51,31 +55,39 @@ func getProducts(w http.ResponseWriter, r *http.Request) {
 	// set structure of what we are going to return
 	products := []Product{}
 
-	// the query we send to db
-	query := `SELECT id, name, description, price FROM products`
-	// query := "SELECT * FROM products"
+	// set the query we send to db
+	query := "SELECT * FROM products"
 
 	// make sure we have cors in the header
 	enableCors(&w)
 
+	// rows = results of the query
 	rows, err := db.Query(query)
+
+	// if there is an error do this
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	// if there is NOT an error do this
 	for rows.Next() {
+		// create a new variable and set its value to our existing struct
 		var product Product
-		err := rows.Scan(&product.ID, &product.Name, &product.Description, &product.Price)
+		err := rows.Scan(&product.ID, &product.Category, &product.Name, &product.Description, &product.Price, &product.ImgMain, &product.ImgB, &product.ImgC)
+		// if there is an error do this
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+		// append the returned values to our products variable
 		products = append(products, product)
 	}
 
+	// let the client know the request worked
 	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json") //let header know it's in a json
+	// let header know it's in a json
+	w.Header().Set("Content-Type", "application/json")
 
 	// return this via json data
 	json.NewEncoder(w).Encode(products)
@@ -84,22 +96,33 @@ func getProducts(w http.ResponseWriter, r *http.Request) {
 
 // Get single product function
 
-// func getProduct(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json") // key
-// 	params := mux.Vars(r)                              // get params
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	productID := params["id"]
+	products := []Product{}
+	query := "SELECT * FROM products WHERE ID = ?"
+	enableCors(&w)
 
-// 	// loop through products and find correct id
+	rows, err := db.Query(query, productID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for rows.Next() {
+		var product Product
+		err := rows.Scan(&product.ID, &product.Category, &product.Name, &product.Description, &product.Price, &product.ImgMain, &product.ImgB, &product.ImgC)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		products = append(products, product)
+	}
 
-// 	for _, item := range products {
-// 		if item.ID == params["id"] {
-// 			json.NewEncoder(w).Encode(item)
-// 			return
-// 		}
-// 	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(products)
 
-// 	json.NewEncoder(w).Encode(&Product{}) // Return the product struct?
-
-// }
+}
 
 func main() {
 
@@ -128,12 +151,12 @@ func main() {
 	for results.Next() {
 		var product Product
 
-		err = results.Scan(&product.ID, &product.Name, &product.Description, &product.Price)
+		err = results.Scan(&product.ID, &product.Category, &product.Name, &product.Description, &product.Price, &product.ImgMain, &product.ImgB, &product.ImgC)
+
 		if err != nil {
 			panic(err.Error())
 		}
 
-		// fmt.Println("Record Returned:")
 		fmt.Println(product)
 	}
 	////////////////////
@@ -145,8 +168,8 @@ func main() {
 
 	// Create route handler - Sets our URL endpoints
 
-	router.HandleFunc("/products", getProducts).Methods("GET") // get all products
-	// router.HandleFunc("/products/{id}", getProduct).Methods("GET") // get single product
+	router.HandleFunc("/products", getProducts).Methods("GET")     // get all products
+	router.HandleFunc("/products/{id}", getProduct).Methods("GET") // get single product
 
 	//  Run server
 
